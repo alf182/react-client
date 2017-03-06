3e8ee8cb47ab358f45be50bc289e5cd2cfb1e6c2
Luis Alfredo <alf.182.u@gmail.com>
2017-03-06 12:47:18 -0400

    Cambios a Template

--- src/modules/Example.jsx
+++ src/modules/Example.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-class Example extends from React.Component{
+class Example extends React.Component{
 	render(){
 		return(
 				<div>This is a little render with reactjs</div>
--- src/routes/Page.jsx
+++ src/routes/Page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import Empleados from '../modules/empleados/Example';
+import Empleados from '../modules/Example';
 
 function Pages() {
   return (
